149a9dde2d73820d99a007bea11966a31cf460f2
OneLiberty <brenner.quentin@gmail.com>
2025-03-26 15:52:30 +0100

    ADD : First itteration of the PlayerController

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum GameMode { Cube, Ship, Wave }
+
+    private GameMode currentGameMode = GameMode.Cube; 
+    private IPlayerMode currentController;
+    private Dictionary<GameMode, IPlayerMode> controllers;
+    
+    private void Awake()
+    {
+        controllers = new Dictionary<GameMode, IPlayerMode> {
+            { GameMode.Cube, new CubeController(this) }
+            // { GameMode.Ship, new ShipController(this) },
+            // { GameMode.Wave, new WaveController(this) }
+        };
+
+        currentController = controllers[currentGameMode];
+    }
+
+    public void ChangeGameMode(GameMode newGameMode) {
+        currentGameMode = newGameMode;
+        currentController = controllers[newGameMode];
+    }
+
+    private void Update()
+    {
+        // Main loop, handles input and other updates
+        currentController.Update();
+    }
+
+    private void FixedUpdate()
+    {
+        // Physics loop
+        currentController.FixedUpdate();
+    }
+
+    public void OnClick(InputValue value)
+    {
+        // Handle click input
+        currentController.OnClick(value);
+    }
+}
+
+public interface IPlayerMode 
+{
+    void Update();
+    void FixedUpdate();
+    void OnClick(InputValue value);
+}
