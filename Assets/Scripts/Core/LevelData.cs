422f0497be1538f449660c68e6e98f731f59a74b
OneLiberty <brenner.quentin@gmail.com>
2025-03-27 12:13:53 +0100

    ADD : Defined LevelData and LevelObjectData

---
+++ Assets/Scripts/Core/LevelData.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class LevelData {
+    public string name;
+    public int levelNumber;
+    public int difficulty; // 1 to 10
+    public string musicFile;
+    public List<LevelObjectData> levelObjects = new List<LevelObjectData>();
+}
+
+[Serializable]
+public class LevelObjectData {
+    public string type;
+    public Vector2 position;
+    public float rotation; 
+    // maybe other properties, we'll see. 
+}
