b09d11f23e30d9d01cad0462ff80d2084a9137a8
BRENNER Quentin <brenner.quentin@gmail.com>
2025-04-03 21:01:41 +0200

    CHG : Added null check and mode change prevention in Portal interaction

--- Assets/Scripts/Items/Portals.cs
+++ Assets/Scripts/Items/Portals.cs
@@ -4,6 +4,9 @@ public class Portal : InteractiveObject
 
     public override void Interact(PlayerController player)
     {
+        if (player == null) return;
+        if (player.currentGameMode == targetMode) return;  // prevent excessive mode change when triggering the collider
+
         player.ChangeGameMode(targetMode);
     }
     // I could add visual and audio effects here
