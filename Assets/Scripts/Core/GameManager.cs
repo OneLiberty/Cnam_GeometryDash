49e67ad8662fc6fcd2c4169c5cccffa95a4da3a7
BRENNER Quentin <brenner.quentin@gmail.com>
2025-04-05 17:29:15 +0200

    ADD : Implemented GameManager class for managing game states and scene transitions

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+    public enum GameState { MainMenu, LevelSlection, Playing, Paused, GameOver, Victory }
+    public GameState CurrentGameState { get; private set; } = GameState.MainMenu;
+
+    [Header("Input Settings")]
+    public InputSettings inputSettings;
+
+    public int CurrentLevel { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartLevel(int levelNumber)
+    {
+        CurrentLevel = levelNumber;
+        CurrentGameState = GameState.Playing;
+
+        SceneManager.LoadScene("Level");
+
+        SceneManager.sceneLoaded += (scene, mode) =>
+        {
+            LevelLoader levelLoader = FindFirstObjectByType<LevelLoader>();
+            if (levelLoader != null)
+            {
+                levelLoader.LoadLevel(levelNumber);
+            }
+        // scene management
+        };
+    } 
+
+    public void PauseGame()
+    {
+        CurrentGameState = GameState.Paused;
+        Time.timeScale = 0f; // Pause the game
+        // scene management
+    }
+
+    public void ReturnToMainMenu()
+    {
+        CurrentGameState = GameState.MainMenu;
+        Time.timeScale = 1f; // Resume the game
+        SceneManager.LoadScene("Main Menu");
+        
+    }
+
+    public void GoToLevelSelection()
+    {
+        CurrentGameState = GameState.LevelSlection;
+        Time.timeScale = 1f; // Resume the game
+        // scene management
+    }
+
+}
