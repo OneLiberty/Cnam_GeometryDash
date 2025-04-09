eac3d724dc7f280389bd4a5275da72fdcc8aabcb
OneLiberty <brenner.quentin@gmail.com>
2025-04-09 16:32:45 +0200

    ADD : Enhance ShipController with particle system integration and improve particle effects in Cube prefab

--- Assets/Scripts/Player/ShipController.cs
+++ Assets/Scripts/Player/ShipController.cs
@@ -4,6 +4,7 @@ public class ShipController : IPlayerMode
 {
     private PlayerController playerController;
     private Rigidbody2D rb;
+    private ParticleSystem particleSystem;
 
     private const float baseSpeed = 10.4f; // this is the default speed in GD (10.4 blocks per second)
     private const float baseGravity = 4f;
@@ -16,7 +17,10 @@ public class ShipController : IPlayerMode
         this.rb = rb;
     }
 
-    public void Initialize(GameObject characterInstance) { }
+    public void Initialize(GameObject characterInstance) 
+    {
+        particleSystem = characterInstance.GetComponentInChildren<ParticleSystem>();
+    }
 
     public void FixedUpdate()
     {
@@ -32,6 +36,8 @@ public class ShipController : IPlayerMode
             rb.linearVelocityY = -10f;
 
         }
+
+        AlignToDirection();
     }
 
     public void Update()
@@ -39,12 +45,23 @@ public class ShipController : IPlayerMode
         if(!playerController.isButtonPressed)
         {
             rb.gravityScale = baseGravity;
+            particleSystem.Stop(true);
+        } 
+        else
+        {
+            particleSystem.Play();
         }
     }
 
     public void OnClick()
     {
         rb.gravityScale = -baseGravity;
+    }
+
+    private void AlignToDirection() 
+    {
+        float angle = Mathf.Atan2(rb.linearVelocityY, rb.linearVelocityX) * Mathf.Rad2Deg;
+        playerController.transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
 
