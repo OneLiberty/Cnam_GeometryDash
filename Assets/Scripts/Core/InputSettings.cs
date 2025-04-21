9b3bc79524ea45b90ea431c6d6867f27554e4ac8
OneLiberty <66571824+OneLiberty@users.noreply.github.com>
2025-04-21 15:26:54 +0200

    REF : Removed uneeded functions in the input settings

--- Assets/Scripts/Core/InputSettings.cs
+++ Assets/Scripts/Core/InputSettings.cs
@@ -5,26 +5,10 @@ using UnityEngine;
 public class InputSettings : MonoBehaviour
 {
     [Header("Input Settings")]
-    [SerializeField] public KeyCode jumpButton_0 { get ; private set; } = KeyCode.Space;
-    [SerializeField] public KeyCode jumpButton_1 { get ; private set; } = KeyCode.Mouse0;
-    [SerializeField] public  KeyCode pauseButton { get ; private set; } = KeyCode.Escape;
-    [SerializeField] public KeyCode restartButton { get ; private set; } = KeyCode.R;
-
-    public void SetJumpBtn(int index, KeyCode key)
-    {
-        if (index == 0) { jumpButton_0 = key; }
-        else if (index == 1) { jumpButton_1 = key; }
-    }
-
-    public void SetPauseBtn(KeyCode key)
-    {
-        pauseButton = key;
-    }
-
-    public void SetRestartBtn(KeyCode key)
-    {
-        restartButton = key;
-    }
+    [SerializeField] public KeyCode jumpButton_0 { get ; set; } = KeyCode.Space;
+    [SerializeField] public KeyCode jumpButton_1 { get ; set; } = KeyCode.Mouse0;
+    [SerializeField] public KeyCode pauseButton { get ; set; } = KeyCode.Escape;
+    [SerializeField] public KeyCode restartButton { get ; set; } = KeyCode.R;
 
     public void ListenForInput(Action<KeyCode> callback) 
     {
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -110,7 +110,7 @@ public class MainMenuManager : UIManager
         button0.onClick.AddListener(() => {
             askInputPanel.SetActive(true);
             GameManager.Instance.inputSettings.ListenForInput(key => {
-                GameManager.Instance.inputSettings.SetJumpBtn(0, key);
+                GameManager.Instance.inputSettings.jumpButton_0 = key;
                 button0text.text = key.ToString();
                 askInputPanel.SetActive(false);
             });
@@ -124,7 +124,7 @@ public class MainMenuManager : UIManager
         button1.onClick.AddListener(() => {
             askInputPanel.SetActive(true);
             GameManager.Instance.inputSettings.ListenForInput(key => {
-                GameManager.Instance.inputSettings.SetJumpBtn(1, key);
+                GameManager.Instance.inputSettings.jumpButton_1 = key;
                 button1text.text = key.ToString();
                 askInputPanel.SetActive(false);
             });
@@ -137,7 +137,7 @@ public class MainMenuManager : UIManager
         restartButton.onClick.AddListener(() => {
             askInputPanel.SetActive(true);
             GameManager.Instance.inputSettings.ListenForInput(key => {
-                GameManager.Instance.inputSettings.SetRestartBtn(key);
+                GameManager.Instance.inputSettings.restartButton = key;
                 restartButtonText.text = key.ToString();
                 askInputPanel.SetActive(false);
             });
@@ -150,7 +150,7 @@ public class MainMenuManager : UIManager
         pauseButton.onClick.AddListener(() => {
             askInputPanel.SetActive(true);
             GameManager.Instance.inputSettings.ListenForInput(key => {
-                GameManager.Instance.inputSettings.SetPauseBtn(key);
+                GameManager.Instance.inputSettings.pauseButton = key;
                 pauseButtonText.text = key.ToString();
                 askInputPanel.SetActive(false);
             });
