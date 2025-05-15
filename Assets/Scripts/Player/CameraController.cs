439a1c83db40bb0913fe3bf70e7aecea57babe29
BRENNER Quentin <brenner.quentin@gmail.com>
2025-05-15 22:26:14 +0200

    CHG : Small adjustment to the camera settings

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -17,10 +17,10 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float parallaxFactor = 0.9f;
     [SerializeField] private Transform backgroundTransform;
 
-    private const float DefaultCameraZoom = 5f;
+    private const float DefaultCameraZoom = 5.5f;
 
     private Vector3 velocity = Vector3.zero;
-    private float lowestY = 2f;
+    private float lowestY = 2.5f;
     private Vector3 lastPosition;
     private Vector2 endPosition;
 
